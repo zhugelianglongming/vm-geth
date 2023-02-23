f3df59edc8881cc527872b4871e921ab1188f6ca
zhugelianglongming <zhugelianglongming@163.com>
2023-02-23 17:51:37 +0800

    Implement stateDB.CreateAccount()

---
+++ geth/address/adaptor.go
@@ -0,0 +1,21 @@
+package address
+
+import "github.com/ethereum/go-ethereum/common"
+
+// Adaptor is address adaptor for Geth EVM implementation
+// which make sure to map random generated contract EVM address to contract name
+type Adaptor struct {
+	contractName string
+	contractAddr common.Address
+}
+
+// BindContract binds newly generated EVM contract address to its name
+func (a *Adaptor) BindContract(name string, addr common.Address) {
+	a.contractName = name
+	a.contractAddr = addr
+}
+
+// E2X transform EVM address to xuper concept
+func (a *Adaptor) E2X(addr common.Address) {
+
+}
